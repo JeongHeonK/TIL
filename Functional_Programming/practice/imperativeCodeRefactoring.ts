92118c4b9fe5127a6b33f82f513990c3917f91cb
김정헌 <metronom7703@naver.com>
2024-10-21 23:40:04 +0900

    feat: 객체지향 및 함수형
    - 이해만 함.. 내가 만들 수 있는가...ㅋㅋㅋ

--- Functional_Programming/practice/imperativeCodeRefactoring.ts
+++ Functional_Programming/practice/imperativeCodeRefactoring.ts
@@ -59,12 +59,12 @@ export async function main() {
   console.log([...gen()]);
 }
 
-function* take(length, iterable) {
+function* take<T>(length: number, iterable: Iterable<T>) {
   const iterator = iterable[Symbol.iterator]();
-  while (--length) {
-    const { done } = iterator.next();
+  while (length-- > 0) {
+    const { done, value } = iterator.next();
     if (done) break;
-    yield iterator.next().value;
+    yield value;
   }
 }
 
@@ -78,8 +78,63 @@ function* chunk<T>(size: number, iterable: Iterable<T>) {
         },
       }),
     ];
-    if (arr.length) yield [...take(size, iterable)];
+    if (arr.length) yield arr;
     if (arr.length < size) break;
-    yield "hi";
   }
+}
+
+function* map<A, B>(
+  f: (a: A) => B,
+  iterable: Iterable<A>
+): IterableIterator<B> {
+  for (const a of iterable) {
+    yield f(a);
+  }
+}
+
+async function fromAsync<T>(iterable: Iterable<Promise<T>>) {
+  const arr: Awaited<T>[] = [];
+  for await (const a of iterable) {
+    arr.push(a);
+  }
+
+  return arr;
+}
+
+async function concurrent2<T>(limit: number, fs: (() => Promise<T>)[]) {
+  const result = await fromAsync(
+    map(
+      (ps) => Promise.all(ps),
+      map((fs) => fs.map((fs) => fs()), chunk(limit, fs))
+    )
+  );
+}
+
+class FxIterator<T> {
+  constructor(public iterable: Iterable<T>) {}
+
+  chunk(size: number) {
+    return fx(chunk(size, this.iterable));
+  }
+
+  map<U>(f: (a: T) => U): FxIterator<U> {
+    return fx(map(f, this.iterable));
+  }
+
+  to<U>(f: (iterable: Iterable<T>) => U): U {
+    return f(this.iterable);
+  }
+}
+
+function fx<T>(iterable: Iterable<T>) {
+  return new FxIterator(iterable);
+}
+
+async function concurrent3<T>(limit: number, fs: (() => Promise<T>)[]) {
+  const result = fx(fs)
+    .chunk(limit)
+    .map((fs) => fs.map((f) => f()))
+    .map((ps) => Promise.all(ps))
+    .to(fromAsync)
+    .then((arr) => arr.flat());
 }
