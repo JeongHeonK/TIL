a24209107b10701fc34e2dd75b73aa05b6fefee0
JeongHeon <kdrlawjdgjs@gmail.com>
2024-11-20 13:49:12 +0900

    feat: UncontrolledForm

--- React-Native/pages/pages/React/design-pattern/react-design/src/App.tsx
+++ React-Native/pages/pages/React/design-pattern/react-design/src/App.tsx
@@ -1,34 +1,9 @@
-import { Props, UserInfo } from "./Components/Server/user-info";
-import { DataSourceWithRender } from "./Components/DataSourceWithRender";
-import axios from "axios";
-import { DataSourceLoader } from "./Components/DataSourceLoader";
-
-const getDataFromServer = async (url: string) => {
-  const response = await axios.get(url);
-
-  return response.data;
-};
-
-const getDataFromLocalStorage = (key: string) => () => {
-  return localStorage.getItem(key);
-};
-
-const Message = ({ msg }: { msg?: string }) => <h1>{msg}</h1>;
+import { UncontrolledForm } from "./Components/UncontrolledForm";
 
 function App() {
   return (
     <main style={{ width: "100%" }}>
-      <DataSourceWithRender
-        getData={() => getDataFromServer("someUrl")}
-        render={(resource: Props["user"]) => <UserInfo user={resource} />}
-      />
-
-      <DataSourceLoader
-        getData={getDataFromLocalStorage("test")}
-        resourceName="msg"
-      >
-        <Message />
-      </DataSourceLoader>
+      <UncontrolledForm />
     </main>
   );
 }
