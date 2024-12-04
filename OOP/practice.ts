669f2bf201060fe8cc9e19e9b1f1fb25b80a37f3
JeongHeon <kdrlawjdgjs@gmail.com>
2024-12-04 21:18:00 +0900

    feat: SRP

--- OOP/practice.ts
+++ OOP/practice.ts
@@ -1,11 +1,69 @@
-class User {
-  constructor(name: string, email: string) {}
+class BlogPost {
+  title: string;
+  content: string;
+
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
 }
 
-class UserAuthentication {
-  constructor(user: User) {}
+class CreatePost {
+  post: BlogPost;
+  constructor(blogPost: BlogPost) {
+    this.post = blogPost;
+  }
 
-  authentication(password: string) {
-    // implementation logic here
+  create() {}
+}
+
+class UpdatePost {
+  post: BlogPost;
+  constructor(blogPost: BlogPost) {
+    this.post = blogPost;
+  }
+
+  update() {}
+}
+
+class DeletePost {
+  post: BlogPost;
+  constructor(blogPost: BlogPost) {
+    this.post = blogPost;
+  }
+
+  delete() {}
+}
+
+class BlogPostDisplay {
+  post: BlogPost;
+  constructor(blogPost: BlogPost) {
+    this.post = blogPost;
+  }
+
+  displayHTML(targetNode: HTMLElement) {
+    const wrapper = document.createElement("div");
+
+    const title = document.createElement("h1");
+    title.textContent = this.post.title;
+
+    const content = document.createElement("p");
+    content.textContent = this.post.content;
+
+    wrapper.append(title);
+    wrapper.append(content);
+
+    return wrapper;
+  }
+}
+
+class BlogPostJson {
+  blogPost: BlogPost;
+  constructor(blogPost: BlogPost) {
+    this.blogPost = blogPost;
+  }
+
+  returnJSON() {
+    return JSON.stringify(this.blogPost);
   }
 }
