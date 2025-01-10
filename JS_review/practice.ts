3ed34d2f03860771db2fbad28ea6381e9b5d5acc
JeongHeon <kdrlawjdgjs@gmail.com>
2025-01-10 12:59:17 +0900

    feat: static initialization

--- JS_review/practice.ts
+++ JS_review/practice.ts
@@ -1,22 +1,5 @@
-class Graph {
-  constructor(protected node: any) {}
-
-  DFSR(start) {
-    const result: any[] = [];
-    const visited = {};
-
-    (function dfs(vertex) {
-      if (!vertex) return;
-
-      visited[vertex] = true;
-      result.push(vertex);
-      this.adjacencyList[vertex].forEach((elem) => {
-        if (!visited[elem]) {
-          dfs(elem);
-        }
-      });
-    })(start);
-
-    return result;
+class MyClass {
+  static {
+    console.log("CLASS IS LOADED");
   }
 }
